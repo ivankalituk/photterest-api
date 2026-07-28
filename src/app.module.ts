10eece775638b4e1c7a7dc9e7c95a0074f3cb55d
ivankalituk <63204841+ivankalituk@users.noreply.github.com>
2026-07-28 16:19:13 +0300

    feat[app]: database init

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from './modules/database/database.module';
+
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+        }),
+
+        DatabaseModule,
+    ],
+
+    controllers: [
+        AppController
+    ],
+
+    providers: [
+        AppService
+    ],
 })
 export class AppModule {}
---
+++ src/modules/database/database.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@nestjs/common';
+import { Pool } from 'pg';
+
+
+@Injectable()
+export class DatabaseService {
+
+    private pool: Pool;
+
+
+    constructor() {
+
+        this.pool = new Pool({
+            host: process.env.DB_HOST,
+            port: Number(process.env.DB_PORT),
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_NAME,
+        });
+
+    }
+
+
+    query(
+        sql: string,
+        params?: any[]
+    ) {
+        return this.pool.query(sql, params);
+    }
+
+}
