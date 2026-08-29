f0abddc106aef0e7d409b1cb0334c0bd3ab6d7de
ivankalituk <63204841+ivankalituk@users.noreply.github.com>
2026-08-29 17:37:11 +0300

    feat[auth]: cookies instead of headers

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -2,7 +2,7 @@ import {
   Body,
   Controller,
   Post,
-  Req,
+  Res,
   UseGuards,
 } from '@nestjs/common';
 
@@ -10,24 +10,60 @@ import { AuthService } from './auth.service';
 import { LoginDTO } from './dto/login.dto';
 import { GoogleAuthDTO } from './dto/googleAuth.dto';
 import { JwtAuthGuard } from './guards/jwt-auth.guard';
+import type { Response } from 'express';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('login')
-  login(@Body() dto: LoginDTO) {
-    return this.authService.login(dto);
+  async login(
+    @Body() dto: LoginDTO,
+    @Res({ passthrough: true }) res: Response,
+  ) {
+    const {user, token} = await this.authService.login(dto);
+
+    res.cookie('access_token', token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: 'lax',
+      path: '/',
+    })
+
+    return{
+      user
+    }
   }
 
   @Post('google')
-  googleLogin(@Body() dto: GoogleAuthDTO) {
-    return this.authService.googleLogin(dto.token);
+  async googleLogin(
+    @Body() dto: GoogleAuthDTO,
+    @Res({ passthrough: true }) res: Response
+  ) {
+    const {user, token} = await this.authService.googleLogin(dto.token);
+
+    res.cookie('access_token', token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: 'lax',
+      path: '/',
+    })
+
+    return{
+      user
+    }
   }
 
   @UseGuards(JwtAuthGuard)
   @Post('logout')
-  logout() {
+  logout(@Res({ passthrough: true }) res: Response) {
+    res.clearCookie('access_token', {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: 'lax',
+      path: '/',
+    });
+
     return this.authService.logOut();
   }
 }
